f519b643d0ec6fde5e0f3f9ffe27e8f46783275c
Toheeb <toheebyekeen@gmail.com>
2022-05-22 22:22:41 +0100

    drag and drop completed

---
+++ src/app.ts
@@ -0,0 +1,6 @@
+import { ProjectList } from "./components/project-list.js";
+import { ProjectInput } from "./components/project-input.js";
+
+new ProjectList("finished");
+new ProjectList("active");
+new ProjectInput();
